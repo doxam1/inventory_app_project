9f55b4269cd514643454eddac11e98c878343849
Doxam1 <143098976+doxam1@users.noreply.github.com>
2024-12-23 17:42:33 +0200

    session secret

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
     store: new (require("connect-pg-simple")(session))({
       store: pool,
     }),
-    secret: "cats",
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
